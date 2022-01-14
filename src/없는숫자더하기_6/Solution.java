d0c2a188712c17a4d59959b67a3d3e09753a0c76
Kyungwoon Na <nakw0410@gmail.com>
2022-01-14 15:03:55 +0900

    feat: 없는숫자 더하기

--- src/없는숫자더하기_6/Solution.java
+++ src/없는숫자더하기_6/Solution.java
@@ -1,2 +1,13 @@
-package 없는숫자더하기_6;public class Solution {
+package 없는숫자더하기_6;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        answer = 45 - sum;
+        return answer;
+    }
 }
