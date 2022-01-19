e7e99111c8c3ad227dd3d0edac71bdb84677f74a
kyungwoon <nakw0410@gmail.com>
2022-01-19 09:10:52 +0900

    feat: 달력 일자 더하기

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,22 @@
-class Main {
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        int [] a = {1,2,3,4};
-        int [] b = {-3,-1,0,2};
-        System.out.println(s.solution(a,b));
+
+public class Main {
+    public int solution(int[] arr1, int[] arr2) {
+        int answer = 0;
+
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr2[i] > 28) {
+                answer += 21 - arr1[i];
+            } else {
+                answer += arr2[i] - arr1[i];
+            }
+        }
+        return answer;
     }
 
-    static class Solution {
-        public int solution(int[] a, int[] b) {
-            int answer = 0;
-
-            for(int i = 0; i < a.length; i++){
-                answer += a[i]*b[i];
-            }
-
-            return answer;
-        }
+    public static void main(String[] args) {
+        Main method = new Main();
+        int[] arr1 = {9, 9, 9, 9, 7, 9, 8};
+        int[] arr2 = {23, 23, 30, 28, 30, 23, 23};
+        System.out.println(method.solution(arr1, arr2));
     }
 }
