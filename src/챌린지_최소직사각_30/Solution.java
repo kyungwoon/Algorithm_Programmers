4929b2e99f718e6cbd415abc674dd5e33ad4acd2
kyungwoon <nakw0410@gmail.com>
2022-01-19 16:40:16 +0900

    feat: 최소 직사각형

--- src/챌린지_최소직사각_30/Solution.java
+++ src/챌린지_최소직사각_30/Solution.java
@@ -3,13 +3,13 @@ package 챌린지_최소직사각_30;
 
 class Solution {
     public int solution(int[][] sizes) {
-        int answer = 0;
+        int answer;
         int maxWidth = 0;
         int maxHight = 0;
 
-        for (int i = 0; i < sizes.length; i++) {
-            int width = Math.max(sizes[i][0], sizes[i][1]);
-            int hight = Math.min(sizes[i][0], sizes[i][1]);
+        for (int[] size : sizes) {
+            int width = Math.max(size[0], size[1]);
+            int hight = Math.min(size[0], size[1]);
             maxWidth = Math.max(maxWidth, width);
             maxHight = Math.max(maxHight, hight);
         }
