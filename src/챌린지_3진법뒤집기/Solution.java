3e6bbe101e60de03e5e8957eebda836d3a3c45b1
kyungwoon <nakw0410@gmail.com>
2022-01-19 15:44:41 +0900

    feat: 3진법 뒤집기

--- src/챌린지_3진법뒤집기/Solution.java
+++ src/챌린지_3진법뒤집기/Solution.java
@@ -4,7 +4,7 @@ class Solution {
     public int solution(int n) {
         String answer = "";
         while (n != 0) {
-            answer += n % 3;
+//            answer += n % 3;
             n /= 3;
         }
         return Integer.parseInt(answer, 3);
