a2619b10cae06d5e24c342dc651640db7ad75c23
kyungwoon <nakw0410@gmail.com>
2022-06-02 19:14:23 +0900

    프로그래머스_짝지어 제거하기

---
+++ src/짝지어제거하기/Solution.java
@@ -0,0 +1,30 @@
+package 짝지어제거하기;
+
+import java.util.Stack;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+
+        Stack<Character> stack = new Stack<>();
+        stack.push(s.charAt(0));
+
+        for (int i = 1; i < s.length(); i++) {
+            if (stack.isEmpty()) {
+                stack.push(s.charAt(i));
+                continue;
+            }
+            if (s.charAt(i) == stack.peek()) {
+                stack.pop();
+            } else {
+                stack.push(s.charAt(i));
+            }
+        }
+        if (stack.isEmpty()) {
+            answer = 1;
+        } else {
+            answer = 0;
+        }
+        return answer;
+    }
+}
